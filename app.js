b71d34759bcbe265d114bd4dae1fce3db623a296
Leonard Pepa <leopepp4@gmail.com>
2021-05-10 05:47:02 +0300

    added  js to read the data from the json files

---
+++ app.js
@@ -0,0 +1,28 @@
+const buildItBtns = document.getElementsByClassName("card-btn");
+const amdBtn = buildItBtns[0];
+const intelBtn = buildItBtns[0];
+
+const cpus = [];
+const hdds = [];
+const gpus = [];
+const motherboards = [];
+const rams = [];
+const powerSupplys = [];
+const cases = [];
+
+const readData = (array, path) => {
+  fetch(`data/${path}.json`)
+    .then((res) => res.json())
+    .then((data) => {
+      data.forEach((item) => array.push(item));
+      return data;
+    });
+};
+
+readData(cpus, "cpus");
+readData(cases, "cases");
+readData(gpus, "gpus");
+readData(hdds, "harddrives");
+readData(motherboards, "motherboards");
+readData(rams, "rams");
+readData(powerSupplys, "power_supplys");
