19fb6735645242243f93858e60e6bf5989ae80ec
Leonard Pepa <leopepp4@gmail.com>
2021-05-13 04:52:10 +0300

    refactored the createContent function to be reusable for all the components

--- build.js
+++ build.js
@@ -5,6 +5,14 @@ const category = document.querySelectorAll(".category");
 const container = document.querySelector(".content-container");
 
 const navLinks = document.querySelectorAll(".nav-link");
+const dataArray = [];
+dataArray.push(cpus);
+dataArray.push(motherboards);
+dataArray.push(cases);
+dataArray.push(powerSupplys);
+dataArray.push(rams);
+dataArray.push(hdds);
+dataArray.push(gpus);
 
 if (!window.name) {
   window.location.replace("index.html");
@@ -42,76 +50,69 @@ const navigateNavBar = () => {
   });
 };
 
-const createContent = () => {
+const createContent = (list, index) => {
   const contentContainer = document.createElement("div");
   contentContainer.classList.add("content-container");
   setTimeout(() => {
-    cpus.forEach((cpu) => {
-      const card = document.createElement("div");
-      card.classList.add("card");
-
-      const div = document.createElement("div");
-      div.innerHTML = `<img class = "card-img" src= "${cpu.poster_path}" alt= "pc part" />`;
-
-      const details = document.createElement("div");
-      details.classList.add("details");
-
-      const name = document.createElement("p");
-      name.classList.add("name");
-
-      name.innerText = `Name : ${cpu.name}`;
-
-      const ul = document.createElement("ul");
-      ul.innerText = "Characteistics";
-
-      const li1 = document.createElement("li");
-      li1.innerText = `Family : ${cpu.characteristics.family}`;
-      const li2 = document.createElement("li");
-      li2.innerText = `Micro architecture : ${cpu.characteristics.microArchitecture}`;
-      const li3 = document.createElement("li");
-      li3.innerText = `Socket : ${cpu.characteristics.socket}`;
-
-      const li4 = document.createElement("li");
-      li4.innerText = `cores : ${cpu.characteristics.cores}`;
-
-      const li5 = document.createElement("li");
-      li5.innerText = `threads : ${cpu.characteristics.threads}`;
-
-      const li6 = document.createElement("li");
-      li6.innerText = `base clock : ${cpu.characteristics.baseClock}`;
-
-      ul.appendChild(li1);
-      ul.appendChild(li2);
-      ul.appendChild(li3);
-      ul.appendChild(li4);
-      ul.appendChild(li5);
-      ul.appendChild(li6);
-
-      const input = document.createElement("input");
-      input.type = "radio";
-      input.classList.add("input");
-      input.name = "price";
-
-      const label = document.createElement("label");
-      label.setAttribute("for", "price");
-      label.classList.add("price-label");
-      label.innerText = `Price : ${cpu.price}€`;
-
-      details.appendChild(name);
-      details.appendChild(ul);
-      details.appendChild(input);
-      details.appendChild(label);
-
-      card.appendChild(div);
-      card.appendChild(details);
-
-      contentContainer.appendChild(card);
-      category[0].appendChild(contentContainer);
+    list.forEach((item) => {
+      createContentCard(item, contentContainer, index);
     });
   }, 100);
 };
 
-createContent();
+const createContentCard = (item, contentContainer, index) => {
+  const card = document.createElement("div");
+  card.classList.add("card");
+
+  const div = document.createElement("div");
+  div.innerHTML = `<img class = "card-img" src= "${item.poster_path}" alt= "pc part" />`;
+
+  const details = document.createElement("div");
+  details.classList.add("details");
+
+  const name = document.createElement("p");
+  name.classList.add("name");
+
+  name.innerText = `Name : ${item.name}`;
+
+  const ul = document.createElement("ul");
+  ul.innerText = "Characteristics";
+
+  for (key in item.characteristics) {
+    const li = document.createElement("li");
+    li.innerText = `${key} : ${item.characteristics[key]}`;
+    ul.appendChild(li);
+  }
+
+  const input = document.createElement("input");
+  input.type = "radio";
+  input.classList.add("input");
+  input.name = "price";
+
+  const label = document.createElement("label");
+  label.setAttribute("for", "price");
+  label.classList.add("price-label");
+  label.innerText = `Price : ${item.price}€`;
+
+  details.appendChild(name);
+  details.appendChild(ul);
+  details.appendChild(input);
+  details.appendChild(label);
+
+  card.appendChild(div);
+  card.appendChild(details);
+
+  contentContainer.appendChild(card);
+  category[index].appendChild(contentContainer);
+};
+
+const displayAllData = () => {
+  for (let i = 0; i < dataArray.length; i++) {
+    createContent(dataArray[i], i);
+  }
+};
+
+displayAllData();
 navigateToHome();
 navigateNavBar();
 configureTitle();
